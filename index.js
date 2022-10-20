43e93a6e5d02d64c1120e82ab6ee08793ddb5b42
Amy Tisdell <amytisdell@icloud.com>
2022-10-19 21:33:25 -0400

    updated to 15:34 mark

--- index.js
+++ index.js
@@ -13,4 +13,29 @@ class Room {
     }
 }
 
-//10 min mark
+class HouseService {
+    static url = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
+
+    static getAllHouses() { 
+        return $.get(this.url);
+    }
+
+    static getHouse(id) {
+        return $.get(this.url + `/${id}`);
+    }
+
+    static createHouse(house) {
+        return $.post(this.url, house); 
+    }
+
+    static updateHouse(house) {
+        return $.ajax({
+            url: this.url + `/${house._id}`;
+            dataType: 'json', 
+            data: JSON.stringify(house),
+            contentType: 'applcation/json',
+            type: 'PUT'
+        });
+    }
+}
+//15.24 mark 
